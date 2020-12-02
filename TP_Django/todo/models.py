0378b4cd44127fb20dce9c4f217d6c7e4cfc828a
papaya-industry <maretvincent@protonmail.com>
2020-12-02 11:36:56 +0100

    add str to Task model

--- TP_Django/todo/models.py
+++ TP_Django/todo/models.py
@@ -5,3 +5,6 @@ class Task(models.Model):
     content = models.CharField(max_length=100)
     is_done = models.BooleanField(default=False)
     created_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.content
