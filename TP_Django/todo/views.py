61b527427b9f4fb93f6ce7ce18714b361d544f49
papaya-industry <maretvincent@protonmail.com>
2020-12-02 11:50:22 +0100

    add edit view

--- TP_Django/todo/views.py
+++ TP_Django/todo/views.py
@@ -6,3 +6,7 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+
+def edit(request, taskId):
+    return HttpResponse("You're editing task %s." % taskId)
