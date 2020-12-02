79bc9ade20e627ae455436a109c03950f2404d55
papaya-industry <maretvincent@protonmail.com>
2020-12-02 13:10:23 +0100

    fix views

--- TP_Django/todo/views.py
+++ TP_Django/todo/views.py
@@ -8,10 +8,10 @@ def index(request):
     '''Return a list of tasks'''
     task_list = Task.objects.all()
     context = {'task_list': task_list}
-    return HttpResponse(render(request, 'todo/index.html', context))
+    return render(request, 'todo/index.html', context)
 
 
 def edit(request, task_id):
     '''Return a task edition page'''
     context = {'task_id': task_id}
-    return HttpResponse(render(request, 'todo/edit.html', context))
+    return render(request, 'todo/edit.html', context)
