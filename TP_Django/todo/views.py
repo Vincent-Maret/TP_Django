789e51550f0216707b90d3f7c399dc5c3ae91d0e
papaya-industry <maretvincent@protonmail.com>
2020-12-02 12:00:55 +0100

    better http response

--- TP_Django/todo/views.py
+++ TP_Django/todo/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse
+from .models import Task
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse(Task.objects.all())
 
 
 def edit(request, taskId):
