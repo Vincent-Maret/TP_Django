1dce6e1de661ea41b4f0b604995c70557ad3a14f
papaya-industry <maretvincent@protonmail.com>
2020-12-02 11:03:53 +0100

    add Task model

--- TP_Django/todo/models.py
+++ TP_Django/todo/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    content = models.CharField(max_length=100)
+    is_done = models.BooleanField(default=False)
+    created_date = models.DateTimeField()
