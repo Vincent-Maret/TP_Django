a7b02cb38bf5b85d4648bf7cce15d8b769090c42
papaya-industry <maretvincent@protonmail.com>
2020-12-02 12:29:56 +0100

    list tasks in index.html

--- TP_Django/todo/views.py
+++ TP_Django/todo/views.py
@@ -1,11 +1,14 @@
 '''Manage django views'''
+from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Task
 
 
 def index(request):
     '''Return a list of tasks'''
-    return HttpResponse(Task.objects.all())
+    task_list = Task.objects.all()
+    context = {'task_list': task_list}
+    return HttpResponse(render(request, 'todo/index.html', context))
 
 
 def edit(request, task_id):
