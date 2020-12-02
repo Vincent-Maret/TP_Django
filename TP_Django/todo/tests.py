bcb251c5da88cd77ac6ecca6bab6d91950900d94
papaya-industry <maretvincent@protonmail.com>
2020-12-02 18:00:47 +0100

    change test file

--- TP_Django/todo/tests.py
+++ TP_Django/todo/tests.py
@@ -1,3 +1,36 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from .models import Task
 
-# Create your tests here.
+
+def create_task(task_text):
+    '''Create a task with given text'''
+    return Task.objects.create(content=task_text)
+
+
+class TaskTests(TestCase):
+    def test_task_creation(self):
+        '''Chould create task test'''
+        c = Client()
+        c.post(reverse('todo:addTask'), {'newTask': 'test'})
+        response = c.get(reverse('todo:index'))
+        self.assertQuerysetEqual(
+            response.context['task_list'], ['<Task: test>'])
+
+    def test_empty_task(self):
+        '''If empty text, task shoudn't be create'''
+        c = Client()
+        c.post(reverse('todo:addTask'), {'newTask': ''})
+        response = c.get(reverse('todo:index'))
+        self.assertQuerysetEqual(
+            response.context['task_list'], [])
+
+    def test_empty_task_update(self):
+        '''If empty text, task shoudn't be create'''
+        c = Client()
+        c.post(reverse('todo:addTask'), {'newTask': 'test'})
+        c.post(reverse('todo:updateTask', kwargs={
+               'task_id': 1}), {'updatedTask': ''})
+        response = c.get(reverse('todo:index'))
+        self.assertQuerysetEqual(
+            response.context['task_list'], ['<Task: test>'])
