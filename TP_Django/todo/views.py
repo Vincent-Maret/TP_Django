e7ecc01234837d0f58a32d3df8c704f40781e67d
papaya-industry <maretvincent@protonmail.com>
2020-12-02 12:13:55 +0100

    add docstrings and fix snake_case convention

--- TP_Django/todo/views.py
+++ TP_Django/todo/views.py
@@ -1,11 +1,13 @@
-from django.shortcuts import render
+'''Manage django views'''
 from django.http import HttpResponse
 from .models import Task
 
 
 def index(request):
+    '''Return a list of tasks'''
     return HttpResponse(Task.objects.all())
 
 
-def edit(request, taskId):
-    return HttpResponse("You're editing task %s." % taskId)
+def edit(request, task_id):
+    '''Return a task edition page'''
+    return HttpResponse("You're editing task %s." % task_id)
